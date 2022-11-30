dfd78eee5c1ea7d1507489bdf626e351d6e2d9be
Nicholas Dylan <dylanlienardi@gmail.com>
2022-11-30 21:33:15 +0700

    initial commit

---
+++ app/src/main/java/com/uc/vpfinalproject/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.uc.vpfinalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uc.vpfinalproject.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
