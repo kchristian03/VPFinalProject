7d5090ba0ae8f116343b6cf1cefdd39b18605c49
Kevin <kchristian03@student.ciputra.ac.id>
2022-12-12 00:18:16 +0700

    update login register

--- app/src/main/java/com/uc/vpfinalproject/MainActivity.kt
+++ app/src/main/java/com/uc/vpfinalproject/MainActivity.kt
@@ -6,16 +6,13 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
 
-@Suppress("DEPRECATION")
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        window.setFlags(
-            FLAG_FULLSCREEN,
-            FLAG_FULLSCREEN
-        )
+
 
         supportActionBar!!.hide()
 
